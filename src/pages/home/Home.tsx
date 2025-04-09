093b5eea41ed09c405cc6e420e9f5c0a2c8ef72e
Azimjon Mamatqulov <mamatqulovazimjon19@gmail.com>
2025-04-09 11:17:20 +0500

    home fixed and last commit

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,28 @@
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+
+const HomeContainer = styled.div((props) => ({
+  height: "100%",
+  background: props.theme.colors.background,
+  display: "grid",
+  placeItems: "center",
+}));
+
+const Button = styled(Link)((props) => ({
+  padding: "50px",
+  display: "grid",
+  placeItems: "center",
+  background: props.theme.colors.secondary,
+  color: props.theme.colors.text,
+  borderRadius: props.theme.borderRadius,
+}));
+
 export const Home = () => {
   return (
     <>
-      <h1>Home</h1>
-      <a href="/calendar"></a>
+      <HomeContainer>
+        <Button to="/calendar">Tabble</Button>
+      </HomeContainer>
     </>
   );
 };
